3c9a5029fa96a9fbd15266ab8bd6efca53cc3fed
Dmi3yy <dmi3yy@gmail.com>
2019-11-05 15:31:28 +0200

    1.4.10

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.9'; // Current version number
-$modx_release_date = 'May 26, 2019'; // Date of release
+$modx_version      = '1.4.10'; // Current version number
+$modx_release_date = 'Nov 05, 2019'; // Date of release
 $modx_branch       = 'Evolution'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
