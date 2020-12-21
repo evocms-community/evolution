96daf89462902fdf8d6404bc8773603d852cc763
Dmi3yy <dmi3yy@gmail.com>
2020-12-21 17:47:29 +0200

    Evolution CMS 1.4.12

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.11'; // Current version number
-$modx_release_date = 'Mar 13, 2020'; // Date of release
+$modx_version      = '1.4.12'; // Current version number
+$modx_release_date = 'Dec 21, 2020'; // Date of release
 $modx_branch       = 'Evolution'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
