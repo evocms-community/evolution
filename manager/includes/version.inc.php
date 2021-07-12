0a5ddf997920a248ae1bfdb4c07b9181f8b006f6
Dmi3yy <dmi3yy@gmail.com>
2021-07-12 14:48:52 +0300

    Evolution CMS 1.4.15

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.14'; // Current version number
-$modx_release_date = 'Mar 9, 2021'; // Date of release
+$modx_version      = '1.4.15'; // Current version number
+$modx_release_date = 'Jul 12, 2021'; // Date of release
 $modx_branch       = 'Evolution'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
