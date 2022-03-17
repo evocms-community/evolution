de58239294fd399bdea59478e517ecf53cd7d1ec
Serg <64j@mail.ru>
2022-03-17 22:49:16 +0300

    [3.x] Fix disabled elements settings

--- core/lang/uk/global.php
+++ core/lang/uk/global.php
@@ -1450,4 +1450,6 @@ $_lang["role_notassigned_tv"] = 'These Variables are available for assigning.';
 $_lang["role_tv_msg"] = 'The Variables assigned to this Role are listed below.';
 $_lang["tmplvar_roles_access_msg"] = 'Select the Roles that are allowed to access/process this Template Variable';
 
+$_lang["setting_from_file"] = '<strong class="text-danger">Значение параметра задано в core/custom/config/cms/settings</strong>';
+
 return $_lang;
