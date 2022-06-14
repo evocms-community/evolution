10d283dfa33e15b65a0131415eee04633f767215
Pathologic <m@xim.name>
2022-06-14 18:12:13 +0300

    update version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version      = '1.4.19'; // Current version number
-$modx_release_date = 'May 9, 2022'; // Date of release
+$modx_release_date = 'June 18, 2022'; // Date of release
 $modx_branch       = 'Evolution CE'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
