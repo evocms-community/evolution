d3377bf7b380193f9819cc35ee20480608d272e7
Pathologic <m@xim.name>
2022-06-26 23:11:27 +0300

    load custom tv default properties from file

--- manager/processors/save_tmplvars.processor.php
+++ manager/processors/save_tmplvars.processor.php
@@ -20,7 +20,7 @@ $locked = isset($_POST['locked']) && $_POST['locked'] == 'on' ? 1 : 0;
 $origin = isset($_REQUEST['or']) ? (int)$_REQUEST['or'] : 76;
 $originId = isset($_REQUEST['oid']) ? (int)$_REQUEST['oid'] : null;
 $currentdate = time() + $modx->config['server_offset_time'];
-$properties = getProperties($type);
+$properties = $_POST['properties'];
 
 //Kyle Jaebker - added category support
 if (empty($_POST['newcategory']) && $_POST['categoryid'] > 0) {
@@ -174,46 +174,4 @@ switch ($_POST['mode']) {
         break;
     default:
         $modx->webAlertAndQuit("No operation set in request.");
-}
-
-function getProperties($type) {
-    // default properties for TV with number type
-    $current = isset($_POST['properties']) ? json_decode($_POST['properties'], true) : null;
-    switch ($type) {
-        case 'number':
-            $properties = $current ?? [
-                'step' => [
-                    [
-                        'label' => 'step',
-                        'type' => 'text',
-                        'value' => '1',
-                        'default' => '1',
-                        'desc' => ''
-                    ]
-                ],
-                'min' => [
-                    [
-                        'label' => 'min',
-                        'type' => 'text',
-                        'value' => '',
-                        'default' => '',
-                        'desc' => ''
-                    ]
-                ],
-                'max' => [
-                    [
-                        'label' => 'max',
-                        'type' => 'text',
-                        'value' => '',
-                        'default' => '',
-                        'desc' => ''
-                    ]
-                ]
-            ];
-            break;
-        default:
-            $properties = [];
-    }
-
-    return $properties;
 }
