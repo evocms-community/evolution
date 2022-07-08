e2532b78fff5e2341396e0b15d2d71cb85157821
webber12 <web-ber12@yandex.ru>
2022-07-08 15:01:31 +0300

    Перевод белорусский

--- manager/includes/lang/errormsg/be.inc.php
+++ manager/includes/lang/errormsg/be.inc.php
@@ -5,34 +5,35 @@
  * @version 1.0.13
  * @date 31/12/2013
  *
- * @language English
+ * @language Belorussian
  * @package modx
  * @subpackage manager
  *
  * Please commit your language changes on Transifex (https://www.transifex.com/projects/p/modx-evolution/) or on GitHub (https://github.com/modxcms/evolution).
  */
-$_lang["No errors occured."] = "No errors occured.";
-$_lang["An error occured!"] = "An error occured!";
-$_lang["You don't have enough privileges for this action!"] = "You don't have enough privileges for this action!";
-$_lang["ID passed in request is NaN!"] = "ID passed in request is NaN!";
-$_lang["The document is locked!"] = "The document is locked!";
-$_lang["Too many results returned from database!"] = "Too many results returned from database!";
-$_lang["Not enough/ no results returned from database!"] = "Not enough/ no results returned from database!";
-$_lang["Couldn't find parent document's name!"] = "Couldn't find parent document's name!";
-$_lang["Logging error!"] = "Logging error!";
-$_lang["Table to optimise not found in request!"] = "Table to optimise not found in request!";
-$_lang["No settings found in request!"] = "No settings found in request!";
-$_lang["The document must have a title!"] = "The document must have a title!";
-$_lang["No user selected as recipient of this message!"] = "No user selected as recipient of this message!";
-$_lang["No group selected as recipient of this message!"] = "No group selected as recipient of this message!";
-$_lang["The document was not found!"] = "The document was not found!";
-$_lang["Double action (GET & POST) posted!"] = "Double action (GET & POST) posted!";
-$_lang["Document cannot be it's own parent!"] = "Document cannot be it's own parent!";
-$_lang["Document's ID not passed in request!"] = "Document's ID not passed in request!";
-$_lang["New parent not set in request!"] = "New parent not set in request!";
-$_lang["don't know the user!" ] = "Incorrect username or password entered or you have been blocked!";
-$_lang["wrong password!"] = "Incorrect username or password entered or you have been blocked!";
-$_lang["Due to too many failed logins, you have been blocked!"] = "Incorrect username or password entered or you have been blocked!";
-$_lang["You are blocked and cannot log in!"] = "Incorrect username or password entered or you have been blocked!";
-$_lang["You are blocked and cannot log in! Please try again later."] = "Incorrect username or password entered or you have been blocked!";
-$_lang["The security code you entered didn't validate! Please try to login again!"] = "The security code you entered didn't validate! Please try to login again!";
+ 
+$_lang["No errors occured."] = 'Памылак не адбылося.'; //No errors occured.
+$_lang["An error occured!"] = 'Адбылася памылка!'; //An error occured!
+$_lang["You don't have enough privileges for this action!"] = 'У вас недастаткова прывілеяў для гэтага дзеяння!'; //You don't have enough privileges for this action!
+$_lang["ID passed in request is NaN!"] = 'ID, перададзены ў запыце, - NaN!'; //ID passed in request is NaN!
+$_lang["The document is locked!"] = 'Дакумент заблакаваны!'; //The document is locked!
+$_lang["Too many results returned from database!"] = 'Занадта шмат вынікаў вернута з базы дадзеных!'; //Too many results returned from database!
+$_lang["Not enough/ no results returned from database!"] = 'Недастаткова / няма вынікаў, вернутых з базы дадзеных!'; //Not enough/ no results returned from database!
+$_lang["Couldn't find parent document's name!"] = 'Немагчыма знайсці назву бацькоўскага дакумента!'; //Couldn't find parent document's name!
+$_lang["Logging error!"] = 'Памылка запісу!'; //Logging error!
+$_lang["Table to optimise not found in request!"] = 'Табліца для аптымізацыі не знойдзена ў запыце!'; //Table to optimise not found in request!
+$_lang["No settings found in request!"] = 'Налады ў запыце не знойдзены!'; //No settings found in request!
+$_lang["The document must have a title!"] = 'Дакумент павінен мець назву!'; //The document must have a title!
+$_lang["No user selected as recipient of this message!"] = 'Ні адзін карыстальнік не выбраны ў якасці атрымальніка гэтага паведамлення!'; //No user selected as recipient of this message!
+$_lang["No group selected as recipient of this message!"] = 'Ні адна група не выбрана ў якасці атрымальніка гэтага паведамлення!'; //No group selected as recipient of this message!
+$_lang["The document was not found!"] = 'Дакумент не знойдзены!'; //The document was not found!
+$_lang["Double action (GET & POST) posted!"] = 'Апублікавана падвойнае дзеянне (GET & POST)!'; //Double action (GET & POST) posted!
+$_lang["Document cannot be it's own parent!"] = 'Дакумент не можа быць уласным бацькам!'; //Document cannot be it's own parent!
+$_lang["Document's ID not passed in request!"] = 'Ідэнтыфікатар дакумента не перададзены ў запыце!'; //Document's ID not passed in request!
+$_lang["New parent not set in request!"] = 'Новы бацька не ўсталяваны ў запыце!'; //New parent not set in request!
+$_lang["don't know the user!"] = 'Уведзены няправільны лагін або пароль, інакш вас заблакіравалі!'; //Incorrect username or password entered or you have been blocked!
+$_lang["wrong password!"] = 'Уведзены няправільны лагін або пароль, інакш вас заблакіравалі!'; //Incorrect username or password entered or you have been blocked!
+$_lang["Due to too many failed logins, you have been blocked!"] = 'Уведзены няправільны лагін або пароль, інакш вас заблакіравалі!'; //Incorrect username or password entered or you have been blocked!
+$_lang["You are blocked and cannot log in!"] = 'Уведзены няправільны лагін або пароль, інакш вас заблакіравалі!'; //Incorrect username or password entered or you have been blocked!
+$_lang["You are blocked and cannot log in! Please try again later."] = 'Уведзены няправільны лагін або пароль, інакш вас заблакіравалі!'; //Incorrect username or password entered or you have been blocked!
+$_lang["The security code you entered didn't validate! Please try to login again!"] = 'Код бяспекі, які вы ўвялі, не спраўдзіўся! Калі ласка, паспрабуйце ўвайсці яшчэ раз!'; //The security code you entered didn't validate! Please try to login again!
