386886f7376490734128cfa64d994dd13dc395f3
Pathologic <m@xim.name>
2022-12-22 07:22:04 +0300

    fix

--- manager/views/frame/tree.blade.php
+++ manager/views/frame/tree.blade.php
@@ -53,6 +53,7 @@ if(is_array($evtOut)) {
         if(is_array($evtOut)) {
             echo implode("\n", $evtOut);
         }
+        $sitename = $modx->getConfig('site_name');
         ?>
 
         <div id="node0" class="rootNode"><a class="node" onclick="modx.tree.treeAction(event, 0)" data-id="0" data-title-esc="{{ $siteName }}"><span class="icon"><i class="{{ $_style['icon_sitemap'] }}"></i></span><span class="title">{{ $siteName }}</span></a>
