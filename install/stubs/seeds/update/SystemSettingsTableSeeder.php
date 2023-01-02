848943232593ab84c7c343e9a85dc3b37fb677a4
Serg <64j@mail.ru>
2023-01-02 11:55:32 +0300

    Fix install: database insert duplicate alias_listing

--- install/stubs/seeds/update/SystemSettingsTableSeeder.php
+++ install/stubs/seeds/update/SystemSettingsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace EvolutionCMS\Installer\Update;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SystemSettingsTableSeeder extends Seeder
 {
@@ -13,35 +14,39 @@ class SystemSettingsTableSeeder extends Seeder
      */
     public function run()
     {
-        $res = \DB::table('system_settings')
+        $res = DB::table('system_settings')
             ->where('setting_name', '=', 'aliaslistingfolder')
             ->orWhere('setting_name', '=', 'full_aliaslisting')
             ->get()
             ->toArray();
 
         $values = [];
+
         foreach ($res as $row) {
             $values[$row->setting_name] = $row->setting_value;
         }
+
         if (isset($values['full_aliaslisting']) || isset($values['aliaslistingfolder'])) {
             $value = 0;
+
             if (isset($values['full_aliaslisting']) && $values['full_aliaslisting'] == 1) {
                 $value = 1;
             } elseif (isset($values['aliaslistingfolder']) && $values['aliaslistingfolder'] == 1) {
                 $value = 2;
             }
 
-            $insertArray = [
-                ['setting_name' => 'alias_listing', 'setting_value' => $value],
-            ];
-            \DB::table('system_settings')->insert($insertArray);
+            DB::table('system_settings')->updateOrInsert(
+                ['setting_name' => 'alias_listing'],
+                ['setting_value' => $value]
+            );
 
             $deleteArray = [
                 'aliaslistingfolder',
                 'full_aliaslisting',
             ];
+
             foreach ($deleteArray as $value) {
-                \DB::table('system_settings')->where('setting_name', $value)->delete();
+                DB::table('system_settings')->where('setting_name', $value)->delete();
             }
         }
     }
