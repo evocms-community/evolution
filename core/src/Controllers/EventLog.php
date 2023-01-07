110814f50822476133a242599e2a86a5f360283b
Oleg Nosov <helgispbru@gmail.com>
2023-01-07 11:19:49 +0300

    исправить context menu в event log

--- core/src/Controllers/EventLog.php
+++ core/src/Controllers/EventLog.php
@@ -1,6 +1,5 @@
 <?php namespace EvolutionCMS\Controllers;
 
-use EvolutionCMS\Models;
 use EvolutionCMS\Interfaces\ManagerTheme;
 
 class EventLog extends AbstractController implements ManagerTheme\PageControllerInterface
@@ -13,18 +12,5 @@ class EventLog extends AbstractController implements ManagerTheme\PageController
     public function canView(): bool
     {
         return $this->managerTheme->getCore()->hasPermission('view_eventlog');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getParameters(array $params = []): array
-    {
-        return [
-            'publishedDocs'   => Models\SiteContent::where('pub_date', '>', time())->orderBy('pub_date', 'asc')->get(),
-            'unpublishedDocs' => Models\SiteContent::where('pub_date', '>', time())->orderBy('unpub_date', 'asc')->get(),
-            'allDocs'         => Models\SiteContent::whereRaw('pub_date > 0 OR unpub_date > 0')->orderBy('pub_date', 'desc')->get(),
-            'server_offset_time' => get_by_key($this->managerTheme->getCore()->config, 'server_offset_time')
-        ];
     }
 }
