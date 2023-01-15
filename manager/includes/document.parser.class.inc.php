e9f47643e5355791c5593bc6bc9bd2332f0f2c31
Yamamoto <yamamoto@kyms.jp>
2023-01-15 22:15:47 +0900

    separate SystemEvent class

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -11,6 +11,7 @@ if (!defined('E_USER_DEPRECATED')) {
     define('E_USER_DEPRECATED', 16384);
 }
 
+include_once __DIR__ . '/system_event.class.inc.php';
 class DocumentParser
 {
     /**
@@ -7024,125 +7025,5 @@ class DocumentParser
             $this->loadExtension('MODxMailer');
         }
         return $this->mail;
-    }
-}
-
-/**
- * System Event Class
- */
-class SystemEvent
-{
-    public $name = '';
-    public $_propagate = true;
-    /**
-     * @deprecated use setOutput(), getOutput()
-     * @var string
-     */
-    public $_output;
-    public $activated = false;
-    public $activePlugin = '';
-    public $params = array();
-
-    /**
-     * Previous event object
-     * @var SystemEvent
-     */
-    private $previousEvent;
-
-    /**
-     * @param string $name Name of the event
-     */
-    public function __construct($name = "")
-    {
-        $this->_resetEventObject();
-        $this->name = $name;
-    }
-
-    /**
-     * Display a message to the user
-     *
-     * @global array $SystemAlertMsgQueque
-     * @param string $msg The message
-     */
-    public function alert($msg)
-    {
-        global $SystemAlertMsgQueque;
-        if ($msg == "") {
-            return;
-        }
-        if (is_array($SystemAlertMsgQueque)) {
-            $title = '';
-            if ($this->name && $this->activePlugin) {
-                $title = "<div><b>" . $this->activePlugin . "</b> - <span style='color:maroon;'>" . $this->name . "</span></div>";
-            }
-            $SystemAlertMsgQueque[] = "$title<div style='margin-left:10px;margin-top:3px;'>$msg</div>";
-        }
-    }
-
-    /**
-     * Output
-     *
-     * @param string $msg
-     * @deprecated see addOutput
-     */
-    public function output($msg)
-    {
-        $this->addOutput($msg);
-    }
-
-    /**
-     * @param mixed $data
-     */
-    public function addOutput($data)
-    {
-        if(\is_scalar($data)) {
-            $this->_output .= $data;
-        }
-    }
-
-    /**
-     * @param mixed $data
-     */
-    public function setOutput($data)
-    {
-        $this->_output = $data;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getOutput()
-    {
-        return $this->_output;
-    }
-
-    /**
-     * Stop event propogation
-     */
-    public function stopPropagation()
-    {
-        $this->_propagate = false;
-    }
-
-    public function _resetEventObject()
-    {
-        unset ($this->returnedValues);
-        $this->name = "";
-        $this->setOutput(null);
-        $this->_propagate = true;
-        $this->activated = false;
-    }
-
-    /**
-     * @param SystemEvent $event
-     */
-    public function setPreviousEvent($event)
-    {
-        $this->previousEvent = $event;
-    }
-
-    public function getPreviousEvent()
-    {
-        return $this->previousEvent;
     }
 }
---
+++ manager/includes/system_event.class.inc.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * System Event Class
+ */
+class SystemEvent
+{
+    public $name = '';
+    public $_propagate = true;
+    /**
+     * @deprecated use setOutput(), getOutput()
+     * @var string
+     */
+    public $_output;
+    public $activated = false;
+    public $activePlugin = '';
+    public $params = [];
+
+    /**
+     * Previous event object
+     * @var SystemEvent
+     */
+    private $previousEvent;
+
+    /**
+     * @param string $name Name of the event
+     */
+    public function __construct($name = "")
+    {
+        $this->_resetEventObject();
+        $this->name = $name;
+    }
+
+    /**
+     * Display a message to the user
+     *
+     * @global array $SystemAlertMsgQueque
+     * @param string $msg The message
+     */
+    public function alert($msg)
+    {
+        global $SystemAlertMsgQueque;
+        if ($msg == "") {
+            return;
+        }
+        if (is_array($SystemAlertMsgQueque)) {
+            $title = '';
+            if ($this->name && $this->activePlugin) {
+                $title = "<div><b>" . $this->activePlugin . "</b> - <span style='color:maroon;'>" . $this->name . "</span></div>";
+            }
+            $SystemAlertMsgQueque[] = "$title<div style='margin-left:10px;margin-top:3px;'>$msg</div>";
+        }
+    }
+
+    /**
+     * Output
+     *
+     * @param string $msg
+     * @deprecated see addOutput
+     */
+    public function output($msg)
+    {
+        $this->addOutput($msg);
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function addOutput($data)
+    {
+        if(\is_scalar($data)) {
+            $this->_output .= $data;
+        }
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public function setOutput($data)
+    {
+        $this->_output = $data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutput()
+    {
+        return $this->_output;
+    }
+
+    /**
+     * Stop event propogation
+     */
+    public function stopPropagation()
+    {
+        $this->_propagate = false;
+    }
+
+    public function _resetEventObject()
+    {
+        unset ($this->returnedValues);
+        $this->name = "";
+        $this->setOutput(null);
+        $this->_propagate = true;
+        $this->activated = false;
+    }
+
+    /**
+     * @param SystemEvent $event
+     */
+    public function setPreviousEvent($event)
+    {
+        $this->previousEvent = $event;
+    }
+
+    public function getPreviousEvent()
+    {
+        return $this->previousEvent;
+    }
+}
