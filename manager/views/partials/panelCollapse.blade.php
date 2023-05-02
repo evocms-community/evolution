8565a1fdc011189fbe0e170e17255a485caa56af
Oleg Nosov <helgispbru@gmail.com>
2023-05-02 08:16:40 +0300

    fix toggle elemens list

--- manager/views/partials/panelCollapse.blade.php
+++ manager/views/partials/panelCollapse.blade.php
@@ -1,11 +1,13 @@
 <div class="panel-heading">
     <span class="panel-title">
-        <a class="accordion-toggle" id="toggle{{ $name }}{{ $id }}" href="#collapse{{ $name }}{{ $id }}" data-cattype="{{ $name }}" data-catid="{{ $id }}" title="Click to toggle collapse. Shift+Click to toggle all.">
+        <a class="accordion-toggle" id="toggle{{ $name }}{{ $id }}"
+            href="#collapse{{ $name }}{{ $id }}" data-cattype="{{ $name }}"
+            data-catid="{{ $id }}" title="Click to toggle collapse. Shift+Click to toggle all.">
             <i class="fa fa-fw"></i>
             <span class="category_name">
                 <strong>
                     {{ $title }}
-                    @if($id > 0)
+                    @if ($id > 0)
                         <small>({{ $id }})</small>
                     @endif
                 </strong>
@@ -13,6 +15,6 @@
         </a>
     </span>
 </div>
-<div id="collapse{{ $name }}{{ $id }}" class="panel-collapse collapse in" aria-expanded="true">
+<div id="collapse{{ $name }}{{ $id }}" class="panel-collapse collapse show">
     {{ $slot }}
 </div>
