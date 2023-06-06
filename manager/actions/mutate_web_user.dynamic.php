9a3a3e0c6b6169d8cc922bb890cd2ddfd8b2f8ec
Serg <64j@mail.ru>
2023-06-06 22:30:00 +0300

    fix #286

--- manager/actions/mutate_web_user.dynamic.php
+++ manager/actions/mutate_web_user.dynamic.php
@@ -1,6 +1,5 @@
 <?php
 use EvolutionCMS\Models\SiteTmplvar;
-use Illuminate\Support\Facades\Request;
 
 if (!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE !== true) {
     die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the EVO Content Manager instead of accessing this file directly.");
