97afc864bd9669322aca9458f3611e9da14c267f
Pathologic <m@xim.name>
2023-06-15 20:23:21 +0300

    update version

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version' => '3.2.0', // Current version number
-    'release_date' => 'May 7, 2023', // Date of release
+    'version' => '3.2.1', // Current version number
+    'release_date' => 'June 15, 2023', // Date of release
     'branch' => 'Evolution CE', // Codebase name
-    'full_appname' => 'Evolution CE 3.2.0 (May 7, 2023)'
+    'full_appname' => 'Evolution CE 3.2.1 (June 15, 2023)'
 ];
