e661a8bfb98fe2032d5850c039fad1b5e158a565
Serg <64j@mail.ru>
2024-06-14 20:47:39 +0300

    Исправление checkPermissions

--- core/src/Legacy/Permissions.php
+++ core/src/Legacy/Permissions.php
@@ -1,4 +1,6 @@
-<?php namespace EvolutionCMS\Legacy;
+<?php
+
+namespace EvolutionCMS\Legacy;
 
 use EvolutionCMS\Models\SiteContent;
 
@@ -11,50 +13,38 @@ class Permissions
      * @var int
      */
     public $user;
+
     /**
      * @var int
      */
     public $document;
+
     /**
      * @var int
      */
     public $role;
+
     /**
      * @var bool
      */
-    public $duplicateDoc = false;
+    public bool $duplicateDoc = false;
 
     /**
      * @return bool
      */
-    public function checkPermissions()
+    public function checkPermissions(): bool
     {
+        $udperms_allowroot = evo()->getConfig('udperms_allowroot', '');
 
-        global $udperms_allowroot;
-        $modx = evolutionCMS();
-
-        $document = $this->document;
-        $role = $this->role;
-
-        if ($role == 1) {
-            return true;  // administrator - grant all document permissions
-        }
-
-        if ($modx->getConfig('use_udperms') == 0 || $modx->getConfig('use_udperms') == "" || !isset($modx->config['use_udperms'])) {
-            return true; // permissions aren't in use
-        }
-        $parent = SiteContent::query()->find($this->document);
-        $parent = $parent->parent ?? null;
-        if ($document == 0 && $parent == null && $udperms_allowroot == 1) {
+        if ($this->role == 1 || !evo()->getConfig('use_udperms') || (!$this->document && $udperms_allowroot == 1)) {
             return true;
-        } // User is allowed to create new document in root
-        if (($this->duplicateDoc == true || $document == 0) && $parent == 0 && $udperms_allowroot == 0) {
-            return false; // deny duplicate || create new document at root if Allow Root is No
         }
 
-        // get document groups for current user
-        $docgrp = empty($_SESSION['mgrDocgroups']) ? '' : implode(' || dg.document_group = ',
-            $_SESSION['mgrDocgroups']);
+        $parent = SiteContent::query()->find($this->document)->parent ?? null;
+
+        if (($this->duplicateDoc || $this->document == 0) && $parent == 0 && $udperms_allowroot == 0) {
+            return false;
+        }
 
         /* Note:
             A document is flagged as private whenever the document group that it
@@ -64,22 +54,21 @@ class Permissions
             are private to the manager users will not be private to web users if the
             document group is not assigned to a web user group and visa versa.
          */
-        $permissionsok = false;  // set permissions to false
 
-        $query = SiteContent::query()->select('id');
-        if(!empty($docgrp)){
-            $query = $query->leftJoin('document_groups', 'site_content.id','=', 'document_groups.document')
-                ->where(function($q) use ($docgrp) {
-                    $q->where('document_groups.document_group', $docgrp)
-                        ->orWhere('site_content.privatemgr', 0);
-                });
-        }else {
-            $query->where('privatemgr', 0);
-        }
-        if ($query->count() > 0) {
-            $permissionsok = true;
-        }
+        $docgrp = implode(
+            ' || dg.document_group = ',
+            $_SESSION['mgrDocgroups'] ?? []
+        );
 
-        return $permissionsok;
+        return SiteContent::query()
+            ->select('id')
+            ->when(
+                $docgrp,
+                fn($query) => $query->leftJoin('document_groups', 'site_content.id', '=', 'document_groups.document')
+                    ->where(fn($q) => $q->where('document_groups.document_group', $docgrp)
+                        ->orWhere('site_content.privatemgr', 0)),
+                fn($query) => $query->where('privatemgr', 0)
+            )
+            ->exists();
     }
 }
