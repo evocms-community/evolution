83973718f0572305d78efd83af9c74a8881b3d22
Serg <64j@mail.ru>
2024-07-02 08:18:33 +0300

    Fix mcpuck

--- manager/media/browser/mcpuk/browser.php
+++ manager/media/browser/mcpuk/browser.php
@@ -1,28 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="Content-Type" content="text/html" />
-	<script>
-		window.opener.KCFinder = {
-			callBack: function(url) {
-				window.opener.KCFinder = null;
-				window.opener.SetUrl(url);
-			}
-		};
-<?php
-	if(isset($_GET['type']))     $type = e(trim($_GET['type']), ENT_QUOTES);
-	elseif(isset($_GET['Type'])) $type = e(trim($_GET['Type']), ENT_QUOTES);
-    else                         $type = 'images';
-    
-    if($type==='image') $type = 'images';
-    
-	$opener = (isset($_GET['editor'])) ? 'opener=' . e(trim($_GET['editor']), ENT_QUOTES) : '';
-	$request_uri = "{$opener}&type={$type}";
-?>
-		window.location.href = "browse.php?<?php echo $request_uri; ?>";
-	</script>
-</head>
+    <meta http-equiv="Content-Type" content="text/html"/>
+    <script type="application/javascript">
+      window.opener.KCFinder = {
+        callBack: function (url) {
+          window.opener.KCFinder = null
+          window.opener.SetUrl(url)
+        }
+      }
+      <?php
+      if (isset($_GET['type'])) {
+          $type = htmlspecialchars(trim($_GET['type']), ENT_QUOTES);
+      } elseif (isset($_GET['Type'])) {
+          $type = htmlspecialchars(trim($_GET['Type']), ENT_QUOTES);
+      } else {
+          $type = 'images';
+      }
 
+      if ($type === 'image') {
+          $type = 'images';
+      }
+
+      $opener = (isset($_GET['editor'])) ? 'opener=' . htmlspecialchars(trim($_GET['editor']), ENT_QUOTES) : '';
+      $request_uri = "$opener&type=$type";
+      ?>
+      window.location.href = "browse.php?<?= $request_uri; ?>"
+    </script>
+</head>
 <body>
 </body>
 </html>
