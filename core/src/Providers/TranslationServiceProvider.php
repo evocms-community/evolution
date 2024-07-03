a3875d6868c4b49f4ec3b2b514fda351b5d8f3fc
Pathologic <m@xim.name>
2024-07-03 15:36:27 +0300

    fix

--- core/src/Providers/TranslationServiceProvider.php
+++ core/src/Providers/TranslationServiceProvider.php
@@ -9,7 +9,19 @@ class TranslationServiceProvider extends \Illuminate\Translation\TranslationServ
     protected function registerLoader()
     {
         $this->app->singleton('translation.loader', function ($app) {
-            return new FileLoader($app['files'], [$app['path.lang'], EVO_CORE_PATH . 'custom/lang/']);
+            return new class($app['files'], [$app['path.lang'], EVO_CORE_PATH . 'custom/lang/']) extends FileLoader {
+                protected function loadPath($path, $locale, $group)
+                {
+                    return collect($path)
+                        ->reduce(function ($output, $_path) use ($locale, $group) {
+                            if ($this->files->exists($full = "{$_path}/{$locale}/{$group}.php")) {
+                                $output = array_replace_recursive($output, $this->files->getRequire($full));
+                            }
+
+                            return $output;
+                        }, []);
+                }
+            };
         });
     }
 }
