02e9defc5cc63998ae1f91e8ebf78ec0f76260f7
Pathologic <m@xim.name>
2024-07-17 05:15:51 +0300

    Update version.php

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version' => '3.1.27', // Current version number
-    'release_date' => 'March 19, 2024', // Date of release
+    'version' => '3.1.28', // Current version number
+    'release_date' => 'July 17, 2024', // Date of release
     'branch' => 'Evolution CE', // Codebase name
-    'full_appname' => 'Evolution CE 3.1.27 (March 19, 2024)'
+    'full_appname' => 'Evolution CE 3.1.28 (July 17, 2024)'
 ];
