89133697f54144038998711fbea61171b8d34993
Pathologic <m@xim.name>
2024-07-17 20:11:03 +0300

    fix custom langs

--- core/src/Providers/TranslationServiceProvider.php
+++ core/src/Providers/TranslationServiceProvider.php
@@ -2,26 +2,14 @@
 
 namespace EvolutionCMS\Providers;
 
-use Illuminate\Translation\FileLoader;
+use EvolutionCMS\Translation\FileLoader;
 
 class TranslationServiceProvider extends \Illuminate\Translation\TranslationServiceProvider
 {
     protected function registerLoader()
     {
         $this->app->singleton('translation.loader', function ($app) {
-            return new class($app['files'], [$app['path.lang'], EVO_CORE_PATH . 'custom/lang/']) extends FileLoader {
-                protected function loadPath($path, $locale, $group)
-                {
-                    return collect($path)
-                        ->reduce(function ($output, $_path) use ($locale, $group) {
-                            if ($this->files->exists($full = "{$_path}/{$locale}/{$group}.php")) {
-                                $output = array_replace_recursive($output, $this->files->getRequire($full));
-                            }
-
-                            return $output;
-                        }, []);
-                }
-            };
+            return new FileLoader($app['files'], [$app['path.lang'], EVO_CORE_PATH . 'custom/lang/']);
         });
     }
 }
