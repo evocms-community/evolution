ac099b82f10ba0d7d67877c10cd6fe9e8f1d6198
Pathologic <m@xim.name>
2024-10-13 01:36:46 +0300

    fix #484

---
+++ install/stubs/migrations/2024_10_13_011111_change_active_users_primary_index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeActiveUsersPrimaryIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('active_users', function (Blueprint $table) {
+            $table->dropPrimary();
+            $table->primary(['sid', 'internalKey']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('active_users', function (Blueprint $table) {
+            $table->dropPrimary();
+            $table->primary('sid');
+        });
+    }
+}
