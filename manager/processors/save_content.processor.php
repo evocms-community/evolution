4dc42c4db6c7ce0978f223469906dddf80f76256
Александр Левченко <seo.supra87@mail.ru>
2025-07-01 14:11:46 +0300

    Update save_content.processor.php

--- manager/processors/save_content.processor.php
+++ manager/processors/save_content.processor.php
@@ -34,7 +34,7 @@ $donthit = (int) $_POST['donthit'];
 $menutitle = $modx->db->escape($_POST['menutitle']);
 $hidemenu = (int) $_POST['hidemenu'];
 $aliasvisible = (int) $_POST['alias_visible'];
-$createdon = strtotime($_POST['createdon']) ?? strtotime('now');
+$createdon = strtotime($_POST['createdon'] ?? 'now');
 
 /************* webber ********/
 $sd = isset($_POST['dir']) && strtolower($_POST['dir']) === 'asc' ? '&dir=ASC' : '&dir=DESC';
