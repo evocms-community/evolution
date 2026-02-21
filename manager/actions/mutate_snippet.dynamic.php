f7959c5dafea7f399efada9b198c28a5f1875165
ProjectSoft <projectsoft2009@yandex.ru>
2026-02-21 16:16:01 +0400

    Update mutate_snippet.dynamic.php

--- manager/actions/mutate_snippet.dynamic.php
+++ manager/actions/mutate_snippet.dynamic.php
@@ -324,6 +324,14 @@ require_once(MODX_MANAGER_PATH . 'includes/active_user_locks.inc.php');
                 }
                 v = arrValues.toString();
                 break;
+			case 'color':
+				var regex = /^(#[0-9a-f]{3}|#(?:[0-9a-f]{2}){2,4}|(?:rgb|hsl)a?\((?:-?\d+%?[,\s]+){2,3}\s*[\d\.]+%?\))$/i,
+					val = ctrl.value;
+				if(regex.test(val)==true){
+					v = val + '';
+				}else{
+					v = "#ffffff";
+				}
             default:
                 v = ctrl.value + '';
                 break;
