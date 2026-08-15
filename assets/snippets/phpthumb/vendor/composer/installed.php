e3909890f01b7c13e6b5742caf5160895f90a2f0
Pathologic <m@xim.name>
2026-08-16 01:38:51 +0300

    update version

--- assets/snippets/phpthumb/vendor/composer/installed.php
+++ assets/snippets/phpthumb/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => '1.4.x-dev',
+        'version' => '1.4.9999999.9999999-dev',
+        'reference' => 'c906b864a2f7c10422c000262390101abcfc0224',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => '1.4.x-dev',
+            'version' => '1.4.9999999.9999999-dev',
+            'reference' => 'c906b864a2f7c10422c000262390101abcfc0224',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
